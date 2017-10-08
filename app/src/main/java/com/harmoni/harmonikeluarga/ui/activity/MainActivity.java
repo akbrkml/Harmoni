453c957383487108f31ea5fdfb12e35a7fbdf2e7
Kamaludin Akbar <akbarmal80@gmail.com>
2017-10-08 11:53:40 +0700

    add activity login

--- app/src/main/java/com/harmoni/harmonikeluarga/ui/activity/MainActivity.java
+++ app/src/main/java/com/harmoni/harmonikeluarga/ui/activity/MainActivity.java
@@ -31,6 +31,8 @@ public class MainActivity extends AppCompatActivity
     private ActionBarDrawerToggle mToggle;
     private FragmentManager fm;
 
+    private boolean mToolBarNavigationListenerIsRegistered = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -64,6 +66,57 @@ public class MainActivity extends AppCompatActivity
         mNavigationView.setNavigationItemSelectedListener(this);
 
         addFragment();
+    }
+
+    /**
+     * To be semantically or contextually correct, maybe change the name
+     * and signature of this function to something like:
+     *
+     * private void showBackButton(boolean show)
+     * Just a suggestion.
+     */
+    private void enableViews(boolean enable) {
+
+        // To keep states of ActionBar and ActionBarDrawerToggle synchronized,
+        // when you enable on one, you disable on the other.
+        // And as you may notice, the order for this operation is disable first, then enable - VERY VERY IMPORTANT.
+        if(enable) {
+            // Remove hamburger
+            mToggle.setDrawerIndicatorEnabled(false);
+            // Show back button
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            // when DrawerToggle is disabled i.e. setDrawerIndicatorEnabled(false), navigation icon
+            // clicks are disabled i.e. the UP button will not work.
+            // We need to add a listener, as in below, so DrawerToggle will forward
+            // click events to this listener.
+            if(!mToolBarNavigationListenerIsRegistered) {
+                mToggle.setToolbarNavigationClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        // Doesn't have to be onBackPressed
+                        onBackPressed();
+                    }
+                });
+
+                mToolBarNavigationListenerIsRegistered = true;
+            }
+
+        } else {
+            // Remove back button
+            getSupportActionBar().setDisplayHomeAsUpEnabled(false);
+            // Show hamburger
+            mToggle.setDrawerIndicatorEnabled(true);
+            // Remove the/any drawer toggle listener
+            mToggle.setToolbarNavigationClickListener(null);
+            mToolBarNavigationListenerIsRegistered = false;
+        }
+
+        // So, one may think "Hmm why not simplify to:
+        // .....
+        // getSupportActionBar().setDisplayHomeAsUpEnabled(enable);
+        // mDrawer.setDrawerIndicatorEnabled(!enable);
+        // ......
+        // To re-iterate, the order in which you enable and disable views IS important #dontSimplify.
     }
 
     private void addFragment(){
--- app/src/main/java/com/harmoni/harmonikeluarga/ui/activity/ProfileActivity.java
+++ app/src/main/java/com/harmoni/harmonikeluarga/ui/activity/ProfileActivity.java
@@ -1,17 +1,31 @@
 package com.harmoni.harmonikeluarga.ui.activity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.harmoni.harmonikeluarga.R;
+import com.harmoni.harmonikeluarga.ui.fragment.ChildFragment;
+import com.harmoni.harmonikeluarga.ui.fragment.HomeFragment;
+import com.harmoni.harmonikeluarga.ui.fragment.ProfileFragment;
+import com.harmoni.harmonikeluarga.ui.fragment.TopicFragment;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class ProfileActivity extends AppCompatActivity {
 
     @BindView(R.id.toolbar)Toolbar mToolbar;
+    @BindView(R.id.img_user)CircleImageView mImageUser;
+    @BindView(R.id.nameUser)TextView mTextUser;
+
+    private FragmentManager fm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,10 +34,49 @@ public class ProfileActivity extends AppCompatActivity {
 
         ButterKnife.bind(this);
 
-        initComponents();
+        initToolbar();
+
+        addFragment();
     }
 
-    private void initComponents(){
-        setSupportActionBar(mToolbar);
+    private void addFragment(){
+        fm = getSupportFragmentManager();
+        fm.beginTransaction().add(R.id.container, new ProfileFragment()).commit();
     }
+
+    private void initToolbar(){
+        setSupportActionBar(mToolbar);
+        setTitle("");
+        mToolbar.setNavigationIcon(R.drawable.ic_toolbar_back);
+        mToolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+    }
+
+    @OnClick(R.id.btn_upload_photo)
+    public void doUpload(){
+
+    }
+
+    @OnClick(R.id.btn_profile)
+    public void goToProfile(){
+        fm = getSupportFragmentManager();
+        fm.beginTransaction().replace(R.id.container, new ProfileFragment()).commit();
+    }
+
+    @OnClick(R.id.btn_anak)
+    public void goToChild(){
+        fm = getSupportFragmentManager();
+        fm.beginTransaction().replace(R.id.container, new ChildFragment()).commit();
+    }
+
+    @OnClick(R.id.btn_topik)
+    public void goToTopic(){
+        fm = getSupportFragmentManager();
+        fm.beginTransaction().replace(R.id.container, new TopicFragment()).commit();
+    }
+
 }
