b5828ee5054841e4d66805da2110a2f951231e6c
akbrkml <akbarmal80@gmail.com>
2018-04-24 11:22:05 +0700

    fix bug

--- app/src/main/java/com/harmoni/harmonikeluarga/ui/fragment/FavoriteFragment.java
+++ app/src/main/java/com/harmoni/harmonikeluarga/ui/fragment/FavoriteFragment.java
@@ -60,7 +60,7 @@ public class FavoriteFragment extends BaseFragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+        // Inflate the layout for this fragment//
         return inflater.inflate(R.layout.fragment_favorite, container, false);
     }
 
